4e5709386d781628e0e419c7d9e6f2b0c5764a4a
Mark Teekman <markteekman@users.noreply.github.com>
2023-01-22 11:03:01 +0100

    Update library output

--- dist/accessible-vue-components.es.js
+++ dist/accessible-vue-components.es.js
@@ -1,24 +1,24 @@
-import { ref as r, openBlock as l, createElementBlock as a, createElementVNode as u, withKeys as k } from "vue";
-const m = ["onKeydown"], p = {
+import { ref as i, openBlock as l, createElementBlock as a, createElementVNode as u, withKeys as k, renderSlot as m, createTextVNode as d } from "vue";
+const p = ["onKeydown"], _ = {
   __name: "SkipLinks",
   setup(s) {
-    const c = r(null), o = (n) => {
-      n.setAttribute("tabindex", "-1"), n.focus();
-    }, i = (n) => {
-      const e = c.value.querySelector("a").getAttribute("href");
-      if (document.querySelector(e)) {
-        const t = document.querySelector(e);
-        o(t);
-      } else if (!document.querySelector(e) && document.querySelector("h1")) {
-        const t = document.querySelector("h1");
-        o(t);
-      } else if (!document.querySelector(e) && !document.querySelector("h1")) {
-        const t = document.querySelector("main");
-        o(t);
+    const c = i(null), o = (e) => {
+      e.setAttribute("tabindex", "-1"), e.focus();
+    }, r = (e) => {
+      const t = c.value.querySelector("a").getAttribute("href");
+      if (document.querySelector(t)) {
+        const n = document.querySelector(t);
+        o(n);
+      } else if (!document.querySelector(t) && document.querySelector("h1")) {
+        const n = document.querySelector("h1");
+        o(n);
+      } else if (!document.querySelector(t) && !document.querySelector("h1")) {
+        const n = document.querySelector("main");
+        o(n);
       } else
         console.warn("SkipLinks are not set, either missing an h1, main landmark or main content id on the page.");
     };
-    return (n, e) => (l(), a("div", {
+    return (e, t) => (l(), a("div", {
       class: "avc-skiplinks",
       ref_key: "skipLinks",
       ref: c
@@ -26,15 +26,19 @@ const m = ["onKeydown"], p = {
       u("a", {
         href: "#main-content",
         class: "skiplinks__skiplink",
-        onKeydown: k(i, ["enter"])
-      }, "Skip to main content", 40, m)
+        onKeydown: k(r, ["enter"])
+      }, [
+        m(e.$slots, "default", {}, () => [
+          d("Skip to main content")
+        ])
+      ], 40, p)
     ], 512));
   }
-}, d = {
+}, S = {
   install: (s) => {
-    s.component("SkipLinks", p);
+    s.component("SkipLinks", _);
   }
 };
 export {
-  d as default
+  S as default
 };
